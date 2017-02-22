ae00d983887a7fcad2f9d43688dc2b932c6ed62d
Benjamin Vetter <vetter@plainpicture.de>
2017-02-22 20:37:03 +0100

    start.sh changed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/usr/bin/java -Xmx128m -jar target/color-thief-1.0.0-jar-with-dependencies.jar 2>&1 | /usr/bin/tee -a log/colorthief.log
+/usr/bin/java -Xmx128m -jar target/color-thief-1.0.0-jar-with-dependencies.jar 2>&1 | /usr/bin/tee -a log/color-thief.log
 
